5bdb01b9066ddd8050f7197e9a14c9aa9b1a2bee
hemershon <hemershon@gmail.com>
2022-02-15 12:54:12 +0000

    Feature/login

--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -5,3 +5,70 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create! email:"graomestre@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"eminente@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"comunicacao@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"pgvigilante@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"sgvigilante@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"secretarioexecutivo@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"secretariati@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+
+Admin.create! email:"loja01@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja02@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja03@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja04@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja05@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja06@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja07@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja08@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja09@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+
+Admin.create! email:"loja10@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja11@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja12@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja13@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja14@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja15@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja16@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja17@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja18@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja19@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+
+Admin.create! email:"loja20@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja21@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja22@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja23@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja24@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja25@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja26@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja27@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja28@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja29@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+
+Admin.create! email:"loja30@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja31@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja32@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja33@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja34@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja35@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja36@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja37@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja38@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja39@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+
+Admin.create! email:"loja40@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja41@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja42@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja43@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja44@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja45@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja46@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja47@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja48@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja49@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+Admin.create! email:"loja50@glmpi.org.br", password: 123456789, created_at: "2020-12-07 07:07:59", updated_at: "2020-12-07 07:07:59"
+
+
+
+
+
