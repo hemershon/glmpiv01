165289da00ea7e2106fa0d764b09fe02cf2ed2e2
hemershon <hemershon@gmail.com>
2022-03-09 22:45:04 +0000

    Feature/balancetes logo v1

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   namespace :backoffice do
     resources :balancesheets do
       root :to => 'balancesheets#index'
-      delete 'balancesheets/:id/' => 'balancesheets#destroy'
     end
     resources :reports do
       root :to => 'reports#index'
